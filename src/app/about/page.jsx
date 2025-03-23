5458db83c663602992d28adb9cf297f3d5eb6fb1
kamikaze-2004 <=>
2025-03-23 21:34:34 +0530

    linkedin and campus intro -- fix

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,7 +4,7 @@ import { TypingText } from "../Components/TypingText";
 
 function About() {
   return (
-    <div className="relative min-h-screen min-w-full text-white">
+    <div className="relative min-h-screen min-w-full text-white overflow-hidden">
       <div className="fixed inset-0 z-0">
         <img 
           src='/images/aurora.jpg'
@@ -15,7 +15,29 @@ function About() {
       </div>
       <div className="relative z-10 pb-10">
         <div className="px-6 py-10">
-          <div className=" mt-20 lg:mt-44 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 items-center mb-8">
+                <div className="flex flex-col  justify-center items-center gap-8 ">
+              
+              <FadeInWhenVisible className="flex justify-center items-center">
+                <img 
+                  src='/images/ceg_front_view.jpg'
+                  alt='campus'
+                  className='h-full w-4/5 rounded-lg ml-8 mt-20 scale-125 md:scale-100 lg:scale-100 md:ml-40 lg:ml-40 '
+                />
+              </FadeInWhenVisible>
+            
+            <div className="flex flex-col justify-start items-center md:items-center lg:items-center">
+              <TypingText classN={'oswald text-5xl lg:md:text-6xl mb-2 font-bold mb-4 '} text={'Anna University'} speed={0.01}/>
+              <TypingText
+                text={
+                  `Established in 1794, Anna University is Asia's oldest technical institution, formed through the merger of four esteemed institutions in Chennai. Offering diverse programs in engineering, technology, architecture, and management, it serves over 12,000 students with nearly 1,000 faculty members. Ranked 10th in India and 427th globally by QS, the university excels in sustainability, securing 9th place in India for sustainability in 2024. It holds an "A++" NAAC accreditation and ranks highly in research and engineering. Anna University is also recognized for its excellence in SDG 7 (Affordable and Clean Energy) and its academic achievements across multiple national rankings.`
+                }
+                speed={0.009} 
+              />
+            </div>
+            </div>
+
+                  <div className=" mt-20 lg:mt-16 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 items-center mb-8">
+
             <div className="flex flex-col justify-start items-center md:items-start lg:items-start">
               <TypingText classN={'oswald text-6xl mb-2 font-bold'} text={'AstroClub'} speed={0.01}/>
               <TypingText classN={'text-xl font-semibold mt-2 mb-4'} text={'Introduction'} speed={0.02}/>
