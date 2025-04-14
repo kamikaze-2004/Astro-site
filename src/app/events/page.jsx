f1d9d9a2a5e20a698119f3fb589dfe7aae2bfc0a
kamikaze-2004 <=>
2025-04-14 23:01:43 +0530

    nothing

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -56,7 +56,7 @@ function Events()
         {
             title:'Astrophotography',
             date:'08/04/25',
-            link:'/images/event/astrophotography.jpg',
+            link:'/images/event/Astrophotography.jpg',
             summary:`As part of Kugelblitz ‘25, the Astro Club and CAC conducted an Astrophotography Workshop led by Mr. D. Vijay Devakumar. He introduced various telescopes used to capture celestial objects and emphasized image and video processing techniques through stacking software. The session was informative, inspiring curiosity and enthusiasm among students to explore the wonders of the night sky.`
         }  
     ];
